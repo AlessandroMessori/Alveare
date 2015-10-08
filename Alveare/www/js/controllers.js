fd4fdc0ae59384f03756d70e5b6e881af8e71cbf
AlessandroMessori <allemex@libero.it>
2015-10-08 18:59:26 +0200

    creata app
    app L'alveare

---
+++ Alveare/www/js/controllers.js
@@ -0,0 +1,24 @@
+angular.module('starter.controllers', [])
+
+
+
+.controller('homeCtrl', function ($scope) {
+
+    $scope.title = "App L'Alveare ";
+    $scope.message = "Benvenuto!,l'app e' ancora in fase di sviluppo ma se ti va puoi fare un giro nelle varie sezioni per avere un'idea delle funzionalita' dell'app completa";
+})
+
+.controller('giornalinoCtrl', function($scope, Chats) {
+    $scope.message = "In questa sezione verranno pubblicati periodicamente contenuti del giornalino scolastico";
+})
+
+
+.controller('orientamentoCtrl', function($scope) {
+    $scope.message = " In questa sezione potrai leggere le esperienze di studenti universitari in modo da avere un'idea di cosa ti aspetta";
+
+})
+
+.controller('forumCtrl', function($scope) {
+    $scope.message = "Questo e' un luogo dove si puo' parlare e discutere di argomenti riguardanti la scuola";
+
+});
