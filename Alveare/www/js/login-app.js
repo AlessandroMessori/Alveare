eff8cf2d8ff0460164e6176af14bfbe8130d06fa
Riccardo Spadaccini <richi.sp29@gmail.com>
2015-10-30 09:44:55 +0100

    Update
    Aggiunto caricamento login (in teoria)

--- Alveare/www/js/login-app.js
+++ Alveare/www/js/login-app.js
@@ -3,6 +3,14 @@ var loginapp = angular.module('login',[]);
 loginapp.controller('loginCtrl', ['$scope', function($scope) {
 
    $scope.UserLogin = function(){Login($scope.username,  $scope.password)};
+   $scope.show = function() {
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+  };
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
 }]);
 
 var signupapp = angular.module('signup',[]);
