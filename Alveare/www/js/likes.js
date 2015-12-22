e9b4120e16ba780bb5195c44301bd51f7ce1d083
AlessandroMessori <allemex@libero.it>
2015-12-22 12:49:41 +0100

    Update
    aggiunti i like

---
+++ Alveare/www/js/likes.js
@@ -0,0 +1,24 @@
+var sendComment = function (liker, father) {
+
+  var Likes = new Parse.Object("Likes");
+
+      Likes.set("Writer", Parse.User.current().get("username"));
+      Likes.set("father", father);
+
+  Likes.save(null, {
+    success: function (Comment) {
+
+    },
+    error: function (Comment, error) {
+    }
+  });
+}
+
+var checklike  = function(liker,father){
+  var Likes = new Parse.Object("Likes");
+
+  var query = new Parse.Query(Likes);
+
+  query.equalTo("father",father);
+
+}
