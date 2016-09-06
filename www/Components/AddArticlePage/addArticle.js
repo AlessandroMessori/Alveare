9fef96ce8b5e8291878b6f306f03c5715c26027b
Riccardo Spadaccini <Riccardo Spadaccini>
2016-09-06 16:01:54 +0200

    Upload immagini storage

--- www/Components/AddArticlePage/addArticle.js
+++ www/Components/AddArticlePage/addArticle.js
@@ -3,6 +3,8 @@ var addArticleCtrl = function ($scope, $window, $ionicLoading, Articles, InputFi
 
     document.getElementById('img-preview').style.display = 'none';
 
+    var imagesRef = Firebase.storage.ref().child('img');
+
     $scope.GetPic = function () {
         navigator.camera.getPicture(onSuccess, onFail, {
             quality: 50,
@@ -29,12 +31,53 @@ var addArticleCtrl = function ($scope, $window, $ionicLoading, Articles, InputFi
                 template: 'Pubblicazione in Corso...'
             });
 
+            var uploadTask = imagesRef.putString($scope.imgData, 'base64url');
+            var downloadURL = null;
+
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                function(snapshot) {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    if ((progress == 25) || (progress == 50) || (progress == 75) || (progress == 100)) {
+                        alert('Upload is ' + progress + '% done');
+                    }
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            alert('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            alert('Upload is running');
+                            break;
+                    }
+                }, function(error) {
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            alert("Utente non autorizzato");
+                            break;
+
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            alert("Upload interrotto dall'utente");
+                            break;
+
+                        case 'storage/unknown':
+                            alert("Errore generico");
+                            // Unknown error occurred, inspect error.serverResponse
+                            break;
+                    }
+                }, function() {
+                    // Upload completed successfully, now we can get the download URL
+                    downloadURL = uploadTask.snapshot.downloadURL;
+                });
+
             var newData = {
                 text: text,
                 title: title,
                 author: Firebase.auth().currentUser.displayName,
                 date: DateHandler.GetCurrentDate(),
-                img: document.getElementById('img_1').src
+                img: downloadURL
             };
 
             Articles.sendArticle(newData);
@@ -42,7 +85,7 @@ var addArticleCtrl = function ($scope, $window, $ionicLoading, Articles, InputFi
             text = '';
         }
         else {
-            alert('compila tutti i campi');
+            alert('Compila tutti i campi');
         }
     };
 
