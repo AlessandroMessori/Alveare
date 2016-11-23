4de22f4678d24f96cfa1d975083e9c73157d653e
5f10 <5f10@Liceospalla.local>
2016-11-23 09:32:25 +0100

    update

--- www/src/App/Services/Data Services/Services/UpdateCred.js
+++ www/src/App/Services/Data Services/Services/UpdateCred.js
@@ -2,7 +2,7 @@ import Firebase from "firebase";
 
 class UpdateCred {
 
-    constructor() {
+    constructor(Modals) {
 
         this.GetUserData = (user, callback) => {
 
@@ -22,9 +22,32 @@ class UpdateCred {
         };
 
         this.Update = (user, email, displayName, password, callback) => {
+            const oldMail = user.email;
 
-            user.updateProfile({email, displayName, password})
-                .then(callback());
+            user.updateProfile({displayName})
+                .then(()=> user.updateEmail(email)
+                    .then(() =>
+                        user.updatePassword(password)
+                            .then(() => {
+                                const ModelRef = Firebase.database().ref("Utenti");
+                                ModelRef.once("value", snapshot => {
+                                    const results = snapshot.val();
+
+                                    if (results != null) {
+                                        Object.keys(results).map(item=> {
+                                            if (results[item].mail.toLowerCase() == oldMail) {
+                                                let updates = results[item];
+                                                updates.mail = email;
+                                                ModelRef.child(item).update(updates).then(callback());
+                                            }
+                                        });
+                                    }
+                                });
+                            })
+                            .catch(()=>Modals.ResultTemplate("La password deve avere almeno 6 caratteri"))
+                    )
+                    .catch(()=>Modals.ResultTemplate("Mail non valida o già in uso da altro account"))
+                )
 
         };
     }
