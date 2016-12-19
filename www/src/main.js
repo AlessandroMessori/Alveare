401e0830f27789075bec554ad45f7284d1e5b6ca
5f10 <5f10@Liceospalla.local>
2016-12-19 12:43:35 +0100

    added fcm service worker

--- www/src/main.js
+++ www/src/main.js
@@ -6,6 +6,17 @@ import Firebase from "firebase";
 import credentials from "../credentials";
 Firebase.initializeApp(credentials);
 
+const messaging = Firebase.messaging();
 
+messaging.requestPermission()
+    .then(() => {
+        console.log('Notification permission granted.');
+        messaging.getToken()
+            .then(currentToken => console.log(currentToken))
+            .catch(err => console.log(err));
+    })
+    .catch((err) => {
+        console.log('Unable to get permission to notify.', err);
+    });
 
 
